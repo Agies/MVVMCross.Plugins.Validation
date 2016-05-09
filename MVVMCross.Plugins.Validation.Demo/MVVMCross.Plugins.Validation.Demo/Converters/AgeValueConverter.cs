0482054c17e0648c68a167d7c923d76cece989de
zhangdiandong <zhangdiandong@sodao.com>
2016-05-09 15:10:43 +0800

    update README.md

---
+++ MVVMCross.Plugins.Validation.Demo/MVVMCross.Plugins.Validation.Demo/Converters/AgeValueConverter.cs
@@ -0,0 +1,20 @@
+﻿using MvvmCross.Platform.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMCross.Plugins.Validation.Demo.Converters
+{
+    public class AgeFormatValueConverter : MvxValueConverter<int, string>
+    {
+        protected override string Convert(int value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == 0)
+                return "";
+
+            return value.ToString();
+        }
+    }
+}
